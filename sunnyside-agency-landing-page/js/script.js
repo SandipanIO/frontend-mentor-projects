4a8641a0c663d5bb90f6b67f58a25d5512f1b156
Sandipan Mukherjee <hello@sandipan.io>
2023-06-20 13:50:20 +0530

    final version

--- sunnyside-agency-landing-page/js/script.js
+++ sunnyside-agency-landing-page/js/script.js
@@ -1,26 +1,55 @@
-const mobileNav = document.querySelector('.main-nav__list');
+/* --------------------------------------------------------------------------- */
+/* Remove Preload class from Body on page load */
+/* --------------------------------------------------------------------------- */
+
+window.addEventListener('load', ()=> {
+   document.querySelector('body').classList.remove('preload');
+});
+
+/* --------------------------------------------------------------------------- */
+/* Mobile Menu */
+/* --------------------------------------------------------------------------- */
+
+const mobileNav = document.querySelector('#primary-navigation');
 const menuButton = document.querySelector('#mobile-menu');
 const lines = menuButton.querySelectorAll('span');
 
-menuButton.addEventListener('click', e => {
+const openMenu = () => {
+   mobileNav.setAttribute('data-visible', 'true');
+   menuButton.setAttribute('aria-expanded', 'true');
+   menuButton.setAttribute('aria-label', 'Close');
+
+   lines.forEach((line, count) => {
+      count++;
+      line.classList.add('main-nav__cross' + count);
+   });
+}
+
+const closeMenu = () => {
+   mobileNav.setAttribute('data-visible', 'false');
+   menuButton.setAttribute('aria-expanded', 'false');
+   menuButton.setAttribute('aria-label', 'Menu');
+
+   lines.forEach((line, count) => {
+      count++;
+      line.classList.remove('main-nav__cross' + count);
+   });
+};
+
+menuButton.addEventListener('click', () => {
    const visibility = mobileNav.getAttribute('data-visible');
 
    if(visibility === "false") {
-      mobileNav.setAttribute('data-visible', 'true');
-      menuButton.setAttribute('aria-expanded', 'true');
-
-      lines.forEach((line, count) => {
-         count++;
-         line.classList.add('main-nav__cross' + count);
-      });
+      openMenu();
 
    } else {
-      mobileNav.setAttribute('data-visible', 'false');
-      menuButton.setAttribute('aria-expanded', 'false');
+      closeMenu();
+   }
+});
 
-      lines.forEach((line, count) => {
-         count++;
-         line.classList.remove('main-nav__cross' + count);
-      });
+/* Close the mobile menu when a link is clicked */
+mobileNav.addEventListener('click', e => {
+   if(e.target.nodeName === 'A' &&  mobileNav.getAttribute('data-visible') === 'true') {
+      closeMenu();
    }
 });
